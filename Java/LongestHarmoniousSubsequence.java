54f574989753d15e95fd2b9fa7a21e0029051302
Simran <simranrauniyar387@gmail.com>
2026-08-31 23:13:16 +0530

    Add Longest Harmonious Subsequence solution

---
+++ Java/LongestHarmoniousSubsequence.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency of each number
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int max = 0;
+
+        // Check pairs x and x + 1
+        for (int num : map.keySet()) {
+            if (map.containsKey(num + 1)) {
+                max = Math.max(max, map.get(num) + map.get(num + 1));
+            }
+        }
+
+        return max;
+    }
+}
